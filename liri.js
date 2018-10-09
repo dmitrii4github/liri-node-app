982a235cdd59515eb072386bbe96ed071f184359
Dmitrii Zakharov <dzakharov@splunk.com>
2018-10-08 20:39:22 -0700

    Done with bands

--- liri.js
+++ liri.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 var Spotify = require("node-spotify-api");
 var keys = require("./keys.js")
 var request = require("request");
+var moment = require("moment");
 
 var spotify = new Spotify(keys.spotify);
 
@@ -33,7 +34,11 @@ function concertThis(operand) {
       
           // Parse the body of the site and recover just the imdbRating
           // (Note: The syntax below for parsing isn't obvious. Just spend a few moments dissecting it).
-          console.log("We received: " + JSON.parse(body)[0].venue.name);
+          console.log("Venue name: " + JSON.parse(body)[0].venue.name);
+          console.log("Venue location: " + JSON.parse(body)[0].venue.city + ", " + JSON.parse(body)[0].venue.region + ", " + JSON.parse(body)[0].venue.country);
+          var datetime = moment(JSON.parse(body)[0].datetime).format("MM/DD/YYYY");
+          console.log("Venue date: " + datetime);
+
         }
       });
 
